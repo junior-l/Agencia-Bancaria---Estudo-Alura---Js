c3af5d6427ba336f070739a1a07d8d49763ed559
Junior Ribeiro <shymxp@gmail.com>
2022-04-20 19:16:03 -0300

    Modularizado código
    Modulos Js e package.json

--- index.js
+++ index.js
@@ -1,31 +1,5 @@
-class Cliente{
-    nome;
-    cpf;
-}
-
-class ContaCorrente{
-    agencia;
-    saldo;
-    
-    sacar(valor){
-        if(this.saldo >= valor){
-            this.saldo -= valor; // retira do saldo o valor que foi sacado
-        }else{
-            console.log("Não Foi possivel fazer esta transação")
-        }
-    }
-
-    depositar(valor){
-        if(valor > 0){
-            this.saldo += valor; // adiciona ao saldo o valor que foi depositado
-            console.log(`Foi depositado o valor de ${valor}`);
-        } else{
-            console.log(`O valor de ${valor} esta incorreto. Refaça a transação.`);
-        }
-    1}
-}
-
-
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
 //clientes
 const cliente1 = new Cliente();
 cliente1.nome = 'Ricardo';
